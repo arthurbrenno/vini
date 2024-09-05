7ebab196046a316ff6a208bacf4f826ac540d495
Arthur Brenno <arthur.coelho@uniube.br>
2024-09-05 17:46:32 +0000

    .

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from openai import OpenAI
 def main():
-    print("Hello from vini!")
+    client = OpenAI(
+        base_url="http://localhost:8000/v1",
+        api_key="token-abc123",
+    )
+
+    completion = client.chat.completions.create(
+    model="NousResearch/Meta-Llama-3-8B-Instruct",
+    messages=[
+        {"role": "user", "content": "Hello!"}
+    ]
+    )
+
+    print(completion.choices[0].message)
 
 
 if __name__ == "__main__":
